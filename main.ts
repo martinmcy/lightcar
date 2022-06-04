c00535ea2615413e4a1e6443175015c77544d19a
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-06-04 10:45:40 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+basic.showNumber(input.lightLevel())
+basic.forever(function () {
+    if (input.lightLevel() > 40) {
+        pins.servoSetPulse(AnalogPin.P1, 180)
+    } else {
+        pins.servoSetPulse(AnalogPin.P1, 0)
+    }
+    if (input.lightLevel() < 40) {
+        pins.servoSetPulse(AnalogPin.P1, 180)
+    } else {
+        pins.servoSetPulse(AnalogPin.P1, 1)
+    }
+    if (input.isGesture(Gesture.LogoDown)) {
+        pins.servoSetPulse(AnalogPin.P1, 0)
+    }
+})
